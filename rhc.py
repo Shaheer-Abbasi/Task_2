14c41bc32c8ebfaa11019b695e8c870497d537f0
Shaheer Abbasi <abbasi.shaheer14@gmail.com>
2026-03-07 14:37:08 -0600

    Fix logic errors/restructure rhc.py file

--- rhc.py
+++ rhc.py
@@ -1,45 +1,54 @@
-import frog
+from frog import frog
 import numpy as np
 
-def RHC(start_point, z, p, seed=67):
-    # Set seed
-    np.random.seed(seed)
-
-    # Evaulate frog for base line position
-    baseline_frog = frog(start_point)
-
-    # Initialize counter to track function evaluations
-    eval_amt = 0
+def get_best_neighbor(curr, z, p):
+    best_neighbor = None
+    best_neighbor_val = float('inf')
 
     # Generate random neighbors
-    for i in range(z):
+    for _ in range(p):
         # Generate random neighbor
-        # Using uniform dist so neighor is within bounds [-z, z]
-        distr = np.random.uniform(-z, z, size=start_point.shape)
-        neighbor = start_point + distr
+        # Using uniform dist so neighbor is within bounds [-z, z]
+        distr = np.random.uniform(-z, z, size=curr.shape)
+        neighbor = curr + distr
 
         # Clamp neighbor to bounds
         # .clip() works by setting values smaller than -512 to -512 and values larger than 512 to 512
         neighbor = np.clip(neighbor, -512, 512)
 
-        eval_neighbor = eval_neighbor(neighbor)
-        eval_amt += 1
+        val = frog(neighbor)
 
-        # Check if new neighbor is smaller than baseline
-        if eval_neighbor is not None:
-            if eval_neighbor < baseline_frog:
-                # Update baseline + start point
-                baseline_frog = eval_neighbor
-                start_point = neighbor
+        # Check if new neighbor is smaller than the best so far
+        # If so, update the best neighbor + value
+        if val < best_neighbor_val:
+            best_neighbor_val = val
+            best_neighbor = neighbor
 
-    return baseline_frog, eval_amt
+    return best_neighbor, best_neighbor_val, p  # p evals were made
 
-def eval_neighbor(neighbor):
-    # Neighbor has z1 and z2 as its coordinates
-    z1, z2 = neighbor
-    # Check maximum bounds
-    if z1 > -512 and z1 < 512 and z2 > -512 and z2 < 512:
-        # Evaluate frog at neighbor position
-        return frog(neighbor)
-    else:
-        return None
+def RHC(start_point, z, p, seed=67):
+    # Set seed
+    np.random.seed(seed)
+
+    # Initialize the current point and the value
+    # Using dtype to ensure truncated integers aren't stored
+    curr = np.array(start_point, dtype=np.float64)
+    curr_val = frog(curr)
+
+    # Initialize counter to track function evaluations
+    # Start at 1 to account for call to frog() above
+    eval_amt = 1
+
+    while True:
+        best_neighbor, best_neighbor_val, evals = get_best_neighbor(curr, z, p)
+        eval_amt += evals
+
+        # Set the current point to the best neighbor found
+        # Or terminate the current solution
+        # And return the current point, value, and number of evaluations
+        if best_neighbor_val < curr_val:
+            curr = best_neighbor
+            curr_val = best_neighbor_val
+        else:
+            return curr, curr_val, eval_amt
+
